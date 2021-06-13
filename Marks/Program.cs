24a31c59d69a2cd982b71ad82ac17e7938ee9b8a
JosephAr1 <jonielhet@yahoo.com>
2021-06-13 19:59:11 +1200

    Add project files.

---
+++ Marks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Marks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mark; 
+            Console.Write("Enter Student Mark: ");
+            mark = Convert.ToInt32(Console.ReadLine());
+
+            if (mark > 91 && mark <= 100)
+            {
+                Console.WriteLine("Student Got A+");
+            }
+            else if(mark>81 && mark <=90)
+            {
+                Console.WriteLine("Student Got A");
+            }
+            else if(mark>71 && mark <=80)
+            {
+                Console.WriteLine("Student Got B");
+            }
+            else if (mark>61 && mark<=70)
+            {
+                Console.WriteLine("Student Got C");
+            }
+            else
+            {
+                Console.WriteLine("Student Failed");
+                Console.ReadKey();
+            }    
+        }
+    }
+}
